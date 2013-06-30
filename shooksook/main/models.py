3f248025721a3b9ebf5351fafd077b45c6c463a2
Gal Wiernik <galw12@cs.huji.ac.il>
2013-06-30 11:24:27 +0300

    Added Templates, changed dircetory for all HTMLs

--- shooksook/main/models.py
+++ shooksook/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length = 50)
+    storeType = models.CharField(max_length = 20)
+    adress = models.CharField(max_length = 50)
+    info = name = models.CharField(max_length = 1000)
+
+    def storeInfo(self):
+        return "Name: " + self.name + '/n' + "Store Type: " + self.storeType + '/n' + "Adress: "+ slef.adress + '/n' + "Info : " +self.info
+
