dd2c035e6b8a2377aab88545b1262aaf019b5f19
Gal Wiernik <galw12@cs.huji.ac.il>
2013-07-01 18:57:44 +0300

    USE THIS

--- shooksook/main/models.py
+++ shooksook/main/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Store(models.Model):
     name = models.CharField(max_length = 50)
     storeType = models.CharField(max_length = 20)
-    adress = models.CharField(max_length = 50)
+    address = models.CharField(max_length = 50)
     info = models.CharField(max_length = 1000)
 
-    def storeInfo(self):
-        return HttpResopnse("Name: " + self.name + '/n' + "Store Type: " + self.storeType + '/n' + "Adress: "+ slef.adress + '/n' + "Info : " +self.info)
+
+##    def storeInfo(self):
+##        return HttpResopnse("Name: " + self.name + '/n' + "Store Type: " + self.storeType + '/n' + "Adress: "+ slef.adress + '/n' + "Info : " +self.info)
 
