ea7c25585df76f11e7ae879f16eef81d9bc0f1ba
Gal Wiernik <galw12@cs.huji.ac.il>
2013-07-03 16:35:26 +0300

    made register work

--- shooksook/main/views.py
+++ shooksook/main/views.py
@@ -1,6 +1,68 @@
 # Create your views here.
 from django.shortcuts import render
+
 from models import Store
+
+from django import forms
+
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserRegistrationForm(forms.Form):
+  username = forms.CharField(label=u'Username')
+  password = forms.CharField(label=u'Password', widget=forms.PasswordInput())
+  ##confirmPS = forms.CharField(label=u'Confirm Password', widget=forms.PasswordInput())
+  email = forms.CharField(label=u'Email')
+  first_name = forms.CharField(label=u'First Name')
+  last_name = forms.CharField(label=u'Last Name')
+
+# Create your views here.
+def showlogin(request):
+  return render(request, 'main/submit/login.html', {'form': UserRegistrationForm()})
+
+def submitlogin(request):
+  Username = request.POST['username']
+  Password = request.POST['password']
+  user = authenticate(username=Username, password=Password)
+  if user is not None:
+    if user.is_active:
+      login(request, user)
+      return HttpResponseRedirect('profile')
+  return HttpResponseRedirect('login')
+
+@login_required
+def profile(request):
+  context = {'username': request.user.username}
+  return render(request, 'main/submit/profile.html', context)
+
+#def log_me_out(request):
+#  logout(request)
+#  return HttpResponseRedirect('/home')
+
+def register(request):
+  if request.method == 'POST':
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid(): ##and form.cleaned_data['password'] == form.cleaned_data['confirmPS']:
+      User.objects.create_user(form.cleaned_data['username'],
+                               form.cleaned_data['email'],
+                               form.cleaned_data['password'],
+                  first_name=form.cleaned_data['first_name'],
+                  last_name=form.cleaned_data['last_name'],)
+      user = authenticate(username=form.cleaned_data['username'],
+                          password=form.cleaned_data['password'])
+      login(request, user)
+      return HttpResponseRedirect('profile')
+    else:
+      return HttpResponseRedirect('media')
+  form = UserRegistrationForm()
+  return render(request, 'main/submit/register.html', {'form': form})
+
+###################################
 
 def home(request):
     return render(request, 'main/dummy.html', {})
@@ -21,11 +83,27 @@ def introduction(request):
 
 def media(request):
 	return render(request, 'main/media.html', {})
+###########################################
 
-def submit(request):
-	return render(request, 'main/submit.html', {})
+@login_required
+def submitBusiness(request):
+	return render(request, 'main/submitBusiness.html', {})
 
-def about(requset):
+def submitBusiness_accepted(request):
+	Name = request.POST['name']
+	StoreType = request.POST['storeType']
+	Address = request.POST['address']
+	Info = request.POST['info']
+	new_store = Store(name = Name, storeType = StoreType, address = Address, info = Info)
+	new_store.save()
+	return HttpResponseRedirect('success')
+
+def success(request):
+	return render(request, 'main/success.html', {})
+
+###################################################
+
+def about(request):
 	return render(request, 'main/about.html', {})
 
 def contact(request):
